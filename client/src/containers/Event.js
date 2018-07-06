cd4fe8a2f19716d4e1a92a206fe52e5a09cb6e43
Josh Bernstein <school.josh@gmail.com>
2018-07-05 19:20:48 -0600

    Added moment.js and  date to event cards

--- client/src/containers/Event.js
+++ client/src/containers/Event.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import _ from 'lodash';
-import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import _ from "lodash";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 const paperStyle = {
-  padding: '10px',
-  margin: '10px'
+  padding: "10px",
+  margin: "10px"
 };
 
 const buttonStyle = {
-  margin: '10px'
+  margin: "10px"
 };
 
 const checkUserGroup = (event, UserGroupUid, userGroup) => {
@@ -21,14 +22,14 @@ const checkUserGroup = (event, UserGroupUid, userGroup) => {
       <div key={UserGroupUid}>
         {/* Why is UserGroupUid for first group exist without name or users? */}
         <Paper style={paperStyle}>
-          {userGroupDetail ? userGroupDetail.Name : ''}
+          {userGroupDetail ? userGroupDetail.Name : ""}
 
           <Button
             variant="contained"
             color="primary"
             style={buttonStyle}
             component={Link}
-            to={event.EventUid + '/' + UserGroupUid}
+            to={event.EventUid + "/" + UserGroupUid}
           >
             View Users
           </Button>
@@ -40,11 +41,31 @@ const checkUserGroup = (event, UserGroupUid, userGroup) => {
   return null;
 };
 
+const dateFormat = (date, type) => {
+  var d = new Date(date);
+
+  switch (type) {
+    case "start":
+      return moment(d).format("MMMM Do YYYY, h:mm:ss a");
+
+    case "end":
+      return moment(d).format("h:mm:ss a");
+
+    default:
+      return null;
+  }
+};
+
 const loadEvent = (event, userGroup) => {
   return (
     <div>
       <Paper style={paperStyle}>
+        {console.log(event)}
         <h1> Event Name: {event.Name} </h1>
+        <h1>
+          Event Time: {dateFormat(event.StartTime, "start")} {" - "}
+          {dateFormat(event.EndTime, "end")}
+        </h1>
         <h2> Event Description: {event.LongDescription} </h2>
         <h3> User Groups: </h3>
 
