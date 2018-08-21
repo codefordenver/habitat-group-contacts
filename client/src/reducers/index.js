8a009db5bcb7977d56215bb330f735e3f3611c2d
David Belisle Jr <resumerequest@designedperfectly.com>
2018-08-20 18:56:39 -0600

    Add AuthReducer.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import AuthReducer from "./reducerAuth";
 import EventReducer from "./reducerEvent";
 import EventsReducer from "./reducerEvents";
 import UserReducer from "./reducerUser";
 import UserGroupReducer from "./reducerUserGroup";
 
 const rootReducer = combineReducers({
+  auth: AuthReducer,
   event: EventReducer,
   events: EventsReducer,
   users: UserReducer,
